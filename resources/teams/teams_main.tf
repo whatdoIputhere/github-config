044bb1d260702cd322eaf3c87ba0f34122ea0af1
whatdoIputhere <pedromgc21@gmail.com>
2024-07-30 15:08:53 +0100

    fix merge main commit

--- resources/teams/teams_main.tf
+++ resources/teams/teams_main.tf
@@ -2,13 +2,10 @@ provider "github" {
   owner = "pecarmoorg"
 }
 
-module "team_api" {
- 
 module api_team {
     source = "../../modules/gh_team"
     team_name = "api"
     team_description = "api team desc"
     team_members = ["whatdoIputhereTEST"]
     team_repo_permissions = "api:push,updated-test-repo:pull"
-    team_repo_permissions = "api:push,frontend:pull"
 }
